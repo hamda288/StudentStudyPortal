b76a082e587f9418cd0a366e21d450a58ce3d7f5
Hamda <96077150+hamda288@users.noreply.github.com>
2023-07-16 17:42:23 +0530

    Update urls.py

--- studentstudyportal/studentstudyportal/urls.py
+++ studentstudyportal/studentstudyportal/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path,include
 from dashboard import views as dash_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('dashboard.urls')),
+    path('',lambda request:redirect('register')),
+    path('home/',include('dashboard.urls')),
     path('register/', dash_views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name="dashboard/login.html"),name='login'),
     path('profile/',dash_views.profile,name='profile'),
